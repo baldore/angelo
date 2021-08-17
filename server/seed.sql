b071a870482ff924449c4a4e440691eb713b15a2
baldore <acornssn@gmail.com>
2021-08-17 08:41:48 -0500

    Update seed.sql

--- server/seed.sql
+++ server/seed.sql
@@ -1,10 +1,15 @@
-begin;
+BEGIN;
 
-insert into songs (name, labels)
-values
-  ('billets doux', '[]'),
-  ('what is this thing called love', '[]'),
+INSERT INTO songs (name, labels)
+VALUES
+  ('billets doux', '[{ "name": "julian concert" }]'),
+  ('what is this thing called love', '[{ "name": "julian concert" }]'),
   ('swing 42', '[{ "name": "julian concert" }]'),
-  ('rhythm futur', '[{ "name": "julian concert" }]');
+  ('songe d''automne', '[{ "name": "julian concert" }]'),
+  ('there will never be another you', '[{ "name": "julian concert" }]'),
+  ('danse novergienne', '[{ "name": "julian concert" }]'),
+  ('rythme futur', '[{ "name": "julian concert" }]'),
+  ('night and day', '[{ "name": "julian concert" }]'),
+  ('minor swing', '[{ "name": "julian concert" }]');
 
-commit;
+COMMIT;
