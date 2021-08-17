ed42c8ef07fc58117ad4ffa0fac086ef19a26ee3
baldore <acornssn@gmail.com>
2021-08-17 08:33:09 -0500

    Add test for GetSong

--- server/db/song_test.go
+++ server/db/song_test.go
@@ -18,21 +18,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var queries *db.Queries
+var (
+	queries  *db.Queries
+	database *sql.DB
+)
+
+const clearSongsQuery = "DELETE FROM songs;"
 
 // Setups dockertest and migrations for tests.
 func TestMain(m *testing.M) {
-	var database *sql.DB
-
-	const databaseName = "angelo"
+	const dbName = "angelo"
 
 	pool, err := dockertest.NewPool("")
 	if err != nil {
 		log.Fatalf("Could not connect to docker: %s", err)
 	}
 
-	resource, err := pool.Run("postgres", "9.6",
-		[]string{"POSTGRES_PASSWORD=secret", "POSTGRES_DB=" + databaseName})
+	resource, err := pool.Run("postgres", "9.6", []string{
+		"POSTGRES_PASSWORD=secret",
+		"POSTGRES_DB=" + dbName,
+	})
 	if err != nil {
 		log.Fatalf("Could not start resource: %s", err)
 	}
@@ -44,7 +49,8 @@ func TestMain(m *testing.M) {
 			fmt.Sprintf(
 				"postgres://postgres:secret@localhost:%s/%s?sslmode=disable",
 				resource.GetPort("5432/tcp"),
-				databaseName))
+				dbName),
+		)
 		if err != nil {
 			return fmt.Errorf("error opening sql connection: %w", err)
 		}
@@ -64,8 +70,7 @@ func TestMain(m *testing.M) {
 	driver, err := postgres.WithInstance(database, &postgres.Config{})
 
 	mig, err := migrate.NewWithDatabaseInstance(
-		"file://../sql/migration",
-		"postgres", driver)
+		"file://../sql/migration", "postgres", driver)
 	if err != nil {
 		log.Fatalf("error creating migration object: %v", err)
 	}
@@ -85,14 +90,41 @@ func TestMain(m *testing.M) {
 }
 
 func TestCreateSong(t *testing.T) {
-	t.Parallel()
-
 	const songName = "ton doux sourire"
 	song, err := queries.CreateSong(context.Background(), songName)
-
 	assert.NoError(t, err, "error creating song: %v", err)
 
 	assert.NotNil(t, song.ID)
 	assert.Equal(t, song.Name, songName)
 	assert.Equal(t, song.Labels, json.RawMessage("[]"))
+}
+
+func TestListSongs(t *testing.T) {
+	ctx := context.Background()
+
+	_, err := database.ExecContext(ctx, clearSongsQuery)
+	assert.NoError(t, err)
+
+	song, err := queries.CreateSong(ctx, "foo")
+	assert.NoError(t, err, "error creating song: %v", err)
+
+	songs, err := queries.ListSongs(ctx)
+	assert.NoError(t, err, "error listing songs: %v", err)
+
+	assert.Equal(t, 1, len(songs))
+	assert.Equal(t, song.Name, songs[0].Name)
+	assert.Equal(t, song.ID, songs[0].ID)
+}
+
+func TestGetSong(t *testing.T) {
+	ctx := context.Background()
+
+	newSong, err := queries.CreateSong(ctx, "song to retrieve")
+	assert.NoError(t, err, "error creating song: %v", err)
+
+	foundSong, err := queries.GetSong(ctx, newSong.ID)
+	assert.NoError(t, err, "error getting song: %v", err)
+
+	assert.Equal(t, newSong.ID, foundSong.ID)
+	assert.Equal(t, newSong.Name, foundSong.Name)
 }
